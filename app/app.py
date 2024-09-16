8a5fb5e2c197d86b4479f0e0ecbdcd1dd02cd588
AoPong-831 <newipadmail114514@gmail.com>
2024-09-16 13:39:06 +0900

    ログイン成功

--- app/app.py
+++ app/app.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from models.models import Chip_database#database読み込み
 from models.database import db_session#inset処理用
 from datetime import datetime#レコードのタイムスタンプ用
-
+from app import key#ログイン用(session管理に使う)のシークレットキー
 
 app = Flask(__name__)
+app.secret_key = key.SECRET_KEY#シークレットキー
+
 
 @app.route("/")
 @app.route("/index",methods=["get","post"])
 def index():
-    if request.method == "post":
-        id = request.form.get("id")#idを受け取る
-        if id == "aaa":#aaaだったらログイン成功
-            return render_template("top2.html")
-    
-    chip_list = Chip_database.query.all()#database読み込み
-    return render_template("top.html",chip_list=chip_list)
+    if "user_name" in session:#ログインしている場合
+        name = session["user_name"]
+        chip_list = Chip_database.query.all()#database読み込み
+        return render_template("top.html",chip_list=chip_list)
+    else:
+        return redirect(url_for("login"))
+
+#ログインページ
+@app.route("/login",methods=["get","post"])
+def login():
+    if request.method == "POST":
+        name = request.form["name"]#user_nameをwebの入力欄から取得
+        user = Chip_database.query.filter_by(name=name).first()#databaseから該当ユーザのレコードを取得
+        if user:#name一致
+            birth = request.form["birth"]
+            if user.birth == birth:#birth一致
+                session["user_name"] = name
+                return redirect(url_for("index"))
+            else:
+                return redirect(url_for("login",status="wrong_password"))
+        else:#name不一致
+            return redirect(url_for("login",status="user_notfound"))
+    else:
+        status = request.args.get("status")#エラーメッセージを受け取る
+        if status == None:status = ""#status空白化
+        return render_template("login.html",status=status)
 
 @app.route("/add",methods=["post"])
 def add():
---
+++ app/key.py
@@ -0,0 +1,2 @@
+SECRET_KEY = "fesfsffesfesaf"#任意の文字列
+SALT = "475474754"#任意の文字列
